e3cc5b10eac297a61323ec934b59ba6b8b0e094c
anisadleena <anisadleena192@gmail.com>
2025-05-23 23:29:33 +0800

    add signup and enhance UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/signup/signup.component';
+import { ForgotPasswordComponent } from './auth/forgotpassword/forgotpassword.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'Home', component: HomeComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -21,6 +21,7 @@ export class AuthService {
   }
 
   signup(data: {
+    username: string;
     email: string;
     password: string;
     [key: string]: any;
